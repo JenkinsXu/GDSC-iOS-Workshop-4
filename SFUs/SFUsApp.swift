7c2a48eb32fd0ff85fe4f664221c74bba0c7c9fd
Jenkins Xu <jenkins@onecup.ai>
2023-11-16 18:19:55 -0800

    Initial Commit

---
+++ SFUs/SFUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFUsApp.swift
+//  SFUs
+//
+//  Created by Yongqi Xu on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct SFUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
